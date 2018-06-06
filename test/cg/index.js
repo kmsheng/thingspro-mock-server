e46c8d88357719dbf9f05704608e769d9fcfbd6a
kmsheng <kmsh3ng@gmail.com>
2018-06-06 11:46:03 +0800

    Test swagger configs (#13)
    * feat: allow program can be run without cb argument provided
    
    * feat: ignore vim swap files
    
    * return express app instance for testing
    
    * feat: add ava@0.25.0 and supertest@3.1.0
    
    * style: re-style ( this is automatically adjusted by yarn )
    
    * feat: specify yarn test command
    
    * feat: new file yarn.lock file
    
    * feat: add test for cg/index.yaml ( GET /network/cellulars )
    
    * feat: using t.snapshot
    
    * feat: commit snapshot files

---
+++ test/cg/index.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import request from 'supertest';
+import createMockServer from '../../index.js';
+import path from 'path';
+
+test.beforeEach(t => {
+
+  const basePath = '/api/v1';
+
+  const app = createMockServer({
+    rootFolderPath: 'schema/cg/',
+    port: 8000,
+    host: '0.0.0.0'
+  });
+
+  t.context.get = (url) => {
+    return request(app)
+      .get(path.join(basePath, url))
+      .set('mx-api-token', '12345678');
+  };
+});
+
+test.cb('GET /network/cellulars', t => {
+
+  t.context.get('/network/cellulars')
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      const [row] = res.body;
+
+      // 'id', 'name', 'mode', 'signal', 'operatorName',
+      // 'lac', 'tac', 'nid', 'cellId', 'bid', 'iccId', 'imsi', 'imei', 'esn',
+      // 'pinRetryRemain', 'status', 'mac', 'ip', 'netmask', 'gateway', 'dns',
+      // 'usage', 'enable', 'pdpContext', 'pinCode', 'keepalive'
+      t.snapshot(Object.keys(row));
+      t.end();
+    });
+});
