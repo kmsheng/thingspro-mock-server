ae2385a7f6aaa1df603b918a80f66d51c2f831f0
kmsheng <kmsh3ng@gmail.com>
2018-06-14 11:56:09 +0800

    test: add file cg/vrrp.js

---
+++ test/cg/vrrp.js
@@ -0,0 +1,48 @@
+import test from 'ava';
+import request from 'supertest';
+import createMockServer from '../../index.js';
+import path from 'path';
+
+test.before(async t => {
+
+  const basePath = '/api/v1';
+  const mxApiToken = '12345678';
+
+  const {app, server} = await createMockServer({
+    rootFolderPath: 'schema/cg/',
+    port: 8000,
+    host: '0.0.0.0',
+    filename: 'vrrp.yaml'
+  });
+
+  t.context.server = server;
+
+  t.context.get = (url) => {
+    return request(app)
+      .get(path.join(basePath, url))
+      .set('mx-api-token', mxApiToken);
+  };
+
+  t.context.put = (url, data) => {
+    return request(app)
+      .put(url)
+      .set('mx-api-token', mxApiToken)
+      .send(data);
+  };
+});
+
+test.cb('GET /network/vrrp', t => {
+
+  t.context.get('/network/vrrp')
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      // result: { enable: false, status: false }
+      t.snapshot(res.body);
+      t.end();
+    });
+});
+
+test.after(t => t.context.server.close());
